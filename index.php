33656b08f23164b39427f4a7a8a78959e8e0c61f
Ryan Mewhorter <rmewhorter16@winona.edu>
2019-07-30 13:04:23 -0500

    .

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css" type="text/css">
+  <title>Keystone Caulking - Home</title>
+
+  <!-- Import FontAwesome -->
+  <script src="https://kit.fontawesome.com/295c776eaf.js"></script>
+</head>
+<body>
+<div class="contents container">
+  <div class="row">
+    <div class="sidebar col-12 col-sm-3 col-xl-2 p-3 container">
+      <header class="row">
+        <img class="logo" src="img/logo_xlg.png" alt="Keystone Caulking logo" />
+      </header>
+      <nav class="row">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#top">
+              <i class="fas fa-home"></i> About Us
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">
+              <i class="fas fa-hammer"></i> Services
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact-us">
+              <i class="fas fa-phone"></i> Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <main class="col-12 col-sm-9 col-xl-10 p-3">
+      <div id="about" class="page row">
+        <!--        <h2 class="col-12">About Us</h2>-->
+        <div class="description col-10 col-sm-7 mx-auto mt-auto mb-4 p-4">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolores earum explicabo hic, minus possimus.</p>
+        </div>
+      </div>
+      <div id="services" class="page container">
+        <h2 class="row">Services</h2>
+        <h5 class="row">We offer the following services:</h5>
+        <div class="row">
+          <ul class="col-12 col-sm-6">
+            <li>Concrete</li>
+            <ul>
+              <li>Control joints</li>
+              <li>Expansion joints</li>
+              <li>Structural cracks</li>
+            </ul>
+            <li>Brick &amp; block control joints</li>
+            <li>Stone control joints</li>
+            <li>Stone-to-stone joints</li>
+          </ul>
+          <ul class="col-12 col-sm-6">
+            <li>Exterior Insulation and Finish System (EIFS)</li>
+            <ul>
+              <li>Control joints</li>
+              <li>Expansion joints</li>
+            </ul>
+            <li>Metal panel joints</li>
+            <li>Cement board/siding joints</li>
+            <li>Door &amp; window frame perimeters</li>
+          </ul>
+        </div>
+      </div>
+      <div id="contact-us" class="page">
+        <h2>Contact Us</h2>
+        <h5 class="mt-4">Get in touch with us directly:</h5>
+        <div id="contact-info" class="d-flex flex-wrap mb-4">
+          <span class="p-2">Joe Mewhorter</span>
+          <span class="p-2">
+            <a href="tel:+15072540665">
+              <i class="fas fa-phone"></i>
+              (507) 254-0665
+            </a>
+          </span>
+          <span class="p-2">
+            <a href="mailto:joe@keystonecaulking.com">
+              <i class="far fa-envelope"></i>
+              joe@keystonecaulking.com
+            </a>
+          </span>
+        </div>
+        <h5>Alternatively, complete the form below and we will contact you.</h5>
+        <form name="contact" action="index.php">
+          <div class="form-group form-row">
+            <input name="name" type="text" class="form-control" id="name" placeholder="Name">
+          </div>
+          <div class="form-group form-row">
+            <div class="col pl-0 pr-2">
+              <input type="tel" name="phone" class="form-control" id="phone" maxlength="11" placeholder="(123) 456-7890">
+            </div>
+            <div class="col pl-2 pr-0">
+              <input name="email" type="email" class="form-control" id="email" placeholder="yourname@example.com">
+            </div>
+          </div>
+          <div class="form-group form-row">
+            <textarea name="message" class="form-control" id="message" cols="30" rows="3" placeholder="Message"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Send email</button>
+        </form>
+      </div>
+      <div class="row p-3">
+        <footer class="col-12 text-secondary">
+          <p class="text-center"><a href="#top">About</a> | <a href="#services">Services</a> | <a href="#contact-us">Contact Us</a></p>
+          <p>Copyright &copy; 2019 Keystone Caulking LLC</p>
+        </footer>
+      </div>
+    </main>
+  </div> <!-- End main content -->
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
